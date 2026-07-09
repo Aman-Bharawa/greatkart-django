d12f2ebb15471087901098842829c47158b2b756
Aman Bharawa <amanbharawa2025@gmail.com>
2026-07-09 22:27:15 +0530

    django authentication

--- greatKart/settings.py
+++ greatKart/settings.py
@@ -134,3 +134,16 @@ MEDIA_ROOT = BASE_DIR /'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
+
+# SMTP configuration
+EMAIL_HOST ='smtp.gmail.com'
+EMAIL_PORT = 587 #for gmail it is 587
+EMAIL_HOST_USER = 'amanbharawa2025@gmail.com'
+EMAIL_HOST_PASSWORD = 'hnsf yvpr fnwb hcbs'
+EMAIL_USE_TLS = True
